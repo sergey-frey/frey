3b3f3de04b0816f7bcc223a74d53303245870fa5
sergey-frey <sergey.frey.work@gmail.com>
2024-09-24 11:54:47 +0300

    feat: add crossed cursor effect and hide header

--- src/shared/ui/cursor/resolvers.ts
+++ src/shared/ui/cursor/resolvers.ts
@@ -1,28 +1,54 @@
 import type { CursorResolverFn } from "@/shared/lib/cursor";
 import { getCssVars } from "@/shared/lib/styles";
 
-export const opacityBgResolver: (options: {
-	p: number;
+export const opacityBgResolver: (options: { p: number }) => CursorResolverFn =
+  ({ p }) =>
+  ({ event, interactiveTarget, cursorMarkerWrapper, cursorMarker }) => {
+    const {
+      left: targetLeft,
+      top: targetTop,
+      width: targetWidth,
+      height: targetHeight,
+    } = interactiveTarget.getBoundingClientRect();
+
+    cursorMarkerWrapper.style.cssText = getCssVars({
+      p: `${p}px`,
+      targetX: `${targetLeft}px`,
+      targetY: `${targetTop}px`,
+      w: `${targetWidth}px`,
+      h: `${targetHeight}px`,
+    });
+
+    cursorMarker.style.cssText = getCssVars({
+      x: `${event.clientX}px`,
+      y: `${event.clientY}px`,
+    });
+  };
+
+export const crossedResolver: (options: {
+  p: number;
+  height: number;
 }) => CursorResolverFn =
-	({ p }) =>
-	({ event, interactiveTarget, cursorMarkerWrapper, cursorMarker }) => {
-		const {
-			left: targetLeft,
-			top: targetTop,
-			width: targetWidth,
-			height: targetHeight,
-		} = interactiveTarget.getBoundingClientRect();
+  ({ p, height }) =>
+  ({ event, interactiveTarget, cursorMarkerWrapper, cursorMarker }) => {
+    const {
+      left: targetLeft,
+      top: targetTop,
+      width: targetWidth,
+      height: targetHeight,
+    } = interactiveTarget.getBoundingClientRect();
 
-		cursorMarkerWrapper.style.cssText = getCssVars({
-			p: `${p}px`,
-			targetX: `${targetLeft}px`,
-			targetY: `${targetTop}px`,
-			w: `${targetWidth}px`,
-			h: `${targetHeight}px`,
-		});
+    cursorMarkerWrapper.style.cssText = getCssVars({
+      p: `${p}px`,
+      targetX: `${targetLeft}px`,
+      targetY: `${targetTop}px`,
+      w: `${targetWidth}px`,
+      h: `${targetHeight}px`,
+      "cursor-height": `${height}px`,
+    });
 
-		cursorMarker.style.cssText = getCssVars({
-			x: `${event.clientX}px`,
-			y: `${event.clientY}px`,
-		});
-	};
+    cursorMarker.style.cssText = getCssVars({
+      x: `${event.clientX}px`,
+      y: `${event.clientY}px`,
+    });
+  };
